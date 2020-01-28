579688250521caabd029e325ba621f22a1cb80eb
pickerColor <60399502+pickerColor@users.noreply.github.com>
2020-01-28 19:44:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var css=document.querySelector("h3");
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+var body=document.getElementById("gradient");
+var button=document.getElementById("randButton");
+
+function setGradient(){
+  body.style.background = 	"linear-gradient(to right, " 	+ color1.value 	+ ", " 	+ color2.value 	+ ")";
+
+  css.textContent=body.style.background +";";
+};
+
+
+color1.addEventListener("input",setGradient);
+
+color2.addEventListener("input",setGradient);
+
+function setRandom(){
+  var letters="0123456789ABCDEF";
+  var color="#";
+  for(var i=0;i<6;i++){
+    color +=letters[Math.floor(Math.random()*16)];
+  }
+  return color;
+};
+
+button.addEventListener("click",function(){
+  body.style.background = 	"linear-gradient(to right, " 	+ setRandom() 	+ ", " 	+ setRandom() 	+ ")";
+  css.textContent=body.style.background +";";
+});
